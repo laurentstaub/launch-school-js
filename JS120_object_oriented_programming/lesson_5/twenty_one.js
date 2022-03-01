24e49ee318490888c3f7c560bfec8b37c82f0667
laurentstaub <laurent.staub@gmail.com>
2022-03-01 09:46:52 +0100

    JS120 Twenty-one v1 without purse

--- JS120_object_oriented_programming/lesson_5/twenty_one.js
+++ JS120_object_oriented_programming/lesson_5/twenty_one.js
@@ -1,39 +1,3 @@
-/*
-Twenty-one is a card game played with 2 players, a deale and a player.
-The winner is the player that gets closer to 21 without going over 21.
-The game starts by dealing cards from a 52-card deck consisting of cards
-  4 suits of 13 ranks each.
-Both players receive 2 cards
-  * The dealer hides one of his cards (face down) so that the player can't
-    see what it is
-  * The player can see both of her cards
-
-The player takes the first turn, and can hit or stay
-  * If the player hits, he gets 1 more card, and can choose to hit or stay
-  * If the total > 21 => bust
-  * If the player stays, the dealer plays next
-
-If the player didn't bust, it's now the dealer's turn
-  * The dealer reveals his face down card
-  * If the dealer's total points are less than 17, he must hit
-  * If the dealer goes over 21, he busts
-  * If the dealer has 17 points or more, he must stay
-
-Results of the game are determined
-
-Nouns: game, player, dealer, participant, turn, deck, card, suit, rank,
-       score, points
-Verbs: start, deal, hit, stay, win, lose, tie, bust, hide, reveal
-
-Game: start
-Deck: deal
-Card
-Participant
-Player: hit, stay, bust(state), score(state)
-Dealer: hit, stay, deal, bust(state), score(state)
-
-*/
-
 const readline = require("readline-sync");
 
 class Card {
@@ -145,9 +109,14 @@ class Participant {
   }
 
   totalScore() {
-    let values = this.hand.map(function(card) {
+    let values = this.hand
+    .filter(function(card) {
+      return card.hidden === false;
+    })
+    .map(function(card) {
       return card.getValue();
     });
+
     let total = 0;
     
     values.forEach(function(value) {
@@ -176,7 +145,7 @@ class Participant {
     for (let index = 0; index <= 9; index += 1) {
       handArray.push([]);
 
-      this.hand.forEach(card => {
+      this.hand.forEach(function(card) {
         let graphicCard = card.createGraphicCard();
         handArray[index].push(graphicCard[index]);
       });
@@ -206,7 +175,7 @@ class TwentyOneGame {
   }
 
   start() {
-    this.displayWelcomeMessage();
+    this.displayBanner();
     this.dealCards();
 
     this.showCards();
@@ -228,9 +197,11 @@ class TwentyOneGame {
   }
 
   showCards() {
+    console.clear();
+    this.displayBanner();
     console.log(`Your cards (${this.player.totalScore()}): `);
     this.player.displayHand();
-    console.log('Dealer cards: ');
+    console.log(`Dealer cards (${this.dealer.totalScore()}): `);
     this.dealer.displayHand();
   }
 
@@ -271,7 +242,7 @@ class TwentyOneGame {
     this.showCards();
   }
 
-  displayWelcomeMessage() {
+  displayBanner() {
     console.log(`
 **************************************************
              This is TWENTY-ONE!
@@ -281,11 +252,12 @@ class TwentyOneGame {
 
   displayGoodbyeMessage() {
     console.log(`Thanks for playing Twenty-One!`);
+    console.log('');
   }
 
   displayResult() {
-    if (this.isBusted(this.player)) console.log('You busted!');
-    else if (this.isBusted(this.dealer)) console.log(('Dealer busted, You win.'));
+    if (this.isBusted(this.player)) console.log(`You busted (${this.player.totalScore()} points)! Dealer wins!`);
+    else if (this.isBusted(this.dealer)) console.log((`Dealer busted (${this.dealer.totalScore()} points)!, You win.`));
     else {
       let playerScore = this.player.totalScore();
       let dealerScore = this.dealer.totalScore();
@@ -298,6 +270,8 @@ class TwentyOneGame {
         console.log(`It's a tie.`);
       }
     }
+
+    console.log('');
   }
 }
 
