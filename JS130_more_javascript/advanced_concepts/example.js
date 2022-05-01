80744e8892fecaec0808b579dd3144ef26732793
laurentstaub <laurent.staub@gmail.com>
2022-05-01 10:32:36 +0200

    JS130 before closures

---
+++ JS130_more_javascript/advanced_concepts/example.js
@@ -0,0 +1,22 @@
+function Pet(name, image) {
+  this.name = name;
+  this.image =  image;
+}
+
+let Image;
+var catImage;
+var pudding;
+
+Image = clas {
+  constructor(file) {
+    this.file = file;
+  }
+}
+
+
+Pet.prototype.walk = function() {
+  console.log(`${this.name} is walking.`);
+};
+
+catImage = new Image("cat.png");
+pudding = new Pet("Pudding", catImage);
--- JS130_more_javascript/todo_list.js
+++ JS130_more_javascript/todo_list.js
@@ -54,24 +54,123 @@ class TodoList {
   last() {
     return this.todos[this.size() - 1];
   }
-}
 
-let list = new TodoList("Today's Todos");
-console.log(list);
+  itemAt(index) {
+    this._validateIndex(index);
+    return this.todos[index];
+  }
+
+  _validateIndex(index) {
+    if (!(index in this.todos)) {
+      throw new ReferenceError(`invalid index: ${index}`);
+    }
+  }
+
+  markDoneAt(index) {
+    this.itemAt(index).markDone();
+  }
+
+  markUndoneAt(index) {
+    this.itemAt(index).markUndone();
+  }
+
+  isDone() {
+    return this.todos.every(todo => todo.isDone());
+  }
+
+  shift() {
+    return this.todos.shift();
+  }
+
+  pop() {
+    return this.todos.pop()
+  }
+
+  removeAt(index) {
+    this._validateIndex(index);
+    return this.todos.splice(index, 1);
+  }
+
+  toString() {
+    console.log(`---- ${this.title} ----`);
+    this.todos.forEach(todo => console.log(todo.toString()));
+  }
+
+  forEach(callback) {
+    this.todos.forEach(callback);
+  }
+
+  filter(callback) {
+    let newList = new TodoList(this.title);
+    this.forEach(todo => {
+      if (callback(todo)) {
+        newList.add(todo);
+      }
+    });
+
+    return newList;
+  }
+
+  findByTitle(title) {
+    return this.filter(todo => todo.getTitle() === title).first();
+  }
+
+  allDone() {
+    return this.filter(todo => todo.isDone() === true);
+  }
+
+  allNotDone() {
+    return this.filter(todo => todo.isDone() === false);
+  }
+
+  markDone(title) {
+    let todo = this.findByTitle(title);
+    if (todo !== undefined) {
+      todo.markDone();
+    }
+  }
+
+  markAllDone() {
+    this.forEach(todo => todo.markDone());
+  }
+
+  markAllUndone() {
+    this.forEach(todo => todo.markUndone());
+  }
+
+  toArray() {
+    return this.todos.slice();
+  }
+}
 
 let todo1 = new Todo("Buy milk");
 let todo2 = new Todo("Clean room");
 let todo3 = new Todo("Go to the gym");
+let todo4 = new Todo("Go shopping");
+let todo5 = new Todo("Feed the cats");
+let todo6 = new Todo("Study for Launch School");
+let list = new TodoList("Today's Todos");
+
 list.add(todo1);
 list.add(todo2);
 list.add(todo3);
-console.log(list);
+list.add(todo4);
+list.add(todo5);
+list.add(todo6);
+todo1.markDone();
+todo5.markDone();
 
-console.log(list.size());
+let doneTodos = list.filter(todo => todo.isDone());
+console.log(doneTodos);
 
-console.log(list.first());
-console.log(list.last());
-
-let emptyList = new TodoList("Empty List");
-console.log(emptyList.first());
-console.log(emptyList.last());
+console.log(list.findByTitle("Feed the cats"));
+console.log(list.allDone());
+console.log(list.allNotDone());
+list.markDone("Clean room");
+console.log(list.allNotDone());
+list.markAllDone();
+console.log(list.allDone());
+list.markAllUndone();
+console.log(list.allDone());
+console.log("Last test")
+console.log(list.toArray());
