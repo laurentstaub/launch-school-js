6cd7a7f23fae544e488276c77189109119895a45
laurentstaub <laurent.staub@gmail.com>
2022-05-03 22:34:58 +0200

    JS130 after mini inventory

--- JS130_more_javascript/exercises/mini_inventory.js
+++ JS130_more_javascript/exercises/mini_inventory.js
@@ -2,7 +2,7 @@ let ItemCreator = (function() {
   let MIN_LENGTH = 5;
 
   function createSkuCode(itemName, category) {
-    return itemName.replace(' ', '').slice(0, 3) + category.slice(0, 2);
+    return (itemName.replace(' ', '').slice(0, 3) + category.slice(0, 2)).toUpperCase();
   }
 
   function isValidItemName(itemName) {
@@ -31,19 +31,58 @@ let ItemCreator = (function() {
 let ItemManager = {
   items: [],
 
+  getItem(skuCode) {
+    return this.items.filter(item => item.skuCode === skuCode)[0];
+  },
+
   create(itemName, category, quantity) {
-    let newItem = new ItemCreator(itemName, category, quantity);
+    const newItem = new ItemCreator(itemName, category, quantity);
     if (newItem.notValid) {
       return false;
     } else this.items.push(newItem);
-    console.log(this.items)
   },
 
-  update(skuCode) {},
-  delete() {},
-  inStock() {},
-  itemsInCategory() {},
+  update(skuCode, obj) {
+    Object.assign(this.getItem(skuCode), obj);
+  },
+  
+  delete(skuCode) {
+    let skuIndex = this.items.indexOf(this.getItem(skuCode));
+    this.items.splice(skuIndex, 1);
+  },
+
+  inStock() {
+    return this.items.filter(item => item.quantity > 0);
+  },
+
+  itemsInCategory(category) {
+    return this.items.filter(item => item.category === category);
+  },
 };
+
+let ReportManager = {
+  init(itemManager) {
+    this.items = itemManager;
+  },
+
+  createReporter(skuCode) {
+    const item = this.items.getItem(skuCode);
+
+    return {
+      itemInfo() {
+        for (let prop in item) {
+          console.log(`${prop}: ${item[prop]}`);
+        }
+      },
+    }
+  },
+
+  reportInStock() {
+    let itemsInStock = [];
+    this.items.inStock().forEach(item => itemsInStock.push(item.itemName));
+    console.log(`${itemsInStock.join(", ")}`)
+  },
+}
 
 // TESTS
 ItemManager.create('basket ball', 'sports', 0);           // valid item
@@ -53,13 +92,11 @@ ItemManager.create('football', 'sports');
 ItemManager.create('football', 'sports', 3);              // valid item
 ItemManager.create('kitchen pot', 'cooking items', 0);
 ItemManager.create('kitchen pot', 'cooking', 3);          // valid item
-
 // returns list with the 4 valid items
 ItemManager.items;
 
 ReportManager.init(ItemManager);
 // logs soccer ball,football,kitchen pot
-/*
 ReportManager.reportInStock();
 
 ItemManager.update('SOCSP', { quantity: 0 });
@@ -84,10 +121,9 @@ kitchenPotReporter.itemInfo();
 // quantity: 3
 
 ItemManager.update('KITCO', { quantity: 10 });
- kitchenPotReporter.itemInfo();
+kitchenPotReporter.itemInfo();
 // logs
 // skuCode: KITCO
 // itemName: kitchen pot
 // category: cooking
 // quantity: 10
-*/
