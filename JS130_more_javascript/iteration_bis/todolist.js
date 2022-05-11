8c3a16eee79bd211e30ec099df77b0ba3384d489
laurentstaub <laurent.staub@gmail.com>
2022-05-11 10:48:25 +0200

    JS130 after todolist problem review

--- JS130_more_javascript/iteration_bis/todolist.js
+++ JS130_more_javascript/iteration_bis/todolist.js
@@ -117,74 +117,78 @@ class TodoList {
   forEach(callback) {
     this.todos.forEach(callback);
   }
+
+  filter(callback) {
+    let filteredTodolist = new TodoList(this.title);
+    filteredTodolist.todos = this.todos.filter(callback);
+    return filteredTodolist;
+  }
+
+  findByTitle(title) {
+    return this.filter(todo => todo.title === title).todos[0];
+  }
+
+  allDone() {
+    return this.filter(todo => todo.isDone());
+  }
+
+  allNotDone() {
+    return this.filter(todo => !todo.isDone());
+  }
+
+  markDone(title) {
+    let titles = [];
+    this.forEach(todo => titles.push(todo.title));
+    this.markDoneAt(titles.indexOf(title));
+  }
+
+  markAllDone() {
+    this.forEach(todo => todo.markDone());
+  }
+
+  markAllUndone() {
+    this.forEach(todo => todo.markUndone());
+  }
+
+  toArray() {
+    return this.todos.slice();
+  }
 }
 
-
-let list = new TodoList("Today's Todos");
-console.log(list);
 
 let todo1 = new Todo("Buy milk");
 let todo2 = new Todo("Clean room");
 let todo3 = new Todo("Go to the gym");
 let todo4 = new Todo("Go shopping");
+let todo5 = new Todo("Feed the cats");
+let todo6 = new Todo("Study for Launch School");
+let list = new TodoList("Today's Todos");
 
 list.add(todo1);
 list.add(todo2);
 list.add(todo3);
 list.add(todo4);
-console.log(list);
-
-console.log(list.size());
-
-console.log(list.first());
-console.log(list.last());
-
-let emptyList = new TodoList("Empty List");
-console.log(emptyList.first());
-console.log(emptyList.last());
-
-list.markDoneAt(1);
-console.log(list);
-
-list.markUndoneAt(1);
-console.log(list);
-
-console.log(list.isDone()); // false
-
-list.markDoneAt(0);
-list.markDoneAt(1);
-list.markDoneAt(2);
-list.markDoneAt(3);
-console.log(list.isDone()); // true
-
-list.markUndoneAt(2);
-console.log(list.isDone()); // false
-
-console.log(list.shift());
-console.log(list.pop());
-console.log(list);
-
-console.log(emptyList.shift());
-console.log(emptyList.pop());
-console.log(emptyList);
-
-// First, let's create some new todos.
-let todo5 = new Todo("Feed the cats");
-let todo6 = new Todo("Study for Launch School");
 list.add(todo5);
 list.add(todo6);
+todo1.markDone();
+todo5.markDone();
+
+let doneTodos = list.filter(todo => todo.isDone());
+console.log(doneTodos);
+
+list.filter(todo => todo.isDone()).first();
+
+console.log(list.findByTitle("Buy milk"));
+console.log(list.findByTitle("Buy milk!"));
+console.log(list.allDone());
+console.log(list.allNotDone());
+list.markDone("Clean room");
+console.log(list.allDone());
+
+list.markAllDone();
 console.log(list);
 
-console.log(list.removeAt(2));
-console.log(list.removeAt(0));
-console.log(list.removeAt(1));
+list.markAllUndone();
 console.log(list);
 
-list.add(todo1);
-list.add(todo2);
-list.add(todo4);
-list.add(todo5);
-list.add(todo6);
-console.log(list.toString());
-
-list.forEach(todo => console.log(todo.toString()));
+console.log(list.toArray());
