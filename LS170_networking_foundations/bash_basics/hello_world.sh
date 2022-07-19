f8b61823ed65eaaa26b1dee02098753e63081103
laurentstaub <laurent.staub@gmail.com>
2022-07-19 18:09:05 +0200

    LS175 halfway through

--- LS170_networking_foundations/bash_basics/hello_world.sh
+++ LS170_networking_foundations/bash_basics/hello_world.sh
@@ -3,6 +3,8 @@
 greeting () {
   echo Hello $1
   echo Hello $2
+  echo Hello $3
+  echo Where is Ringo?
 }
 
 greeting 'George' 'Paul' 'John'
---
+++ LS170_networking_foundations/bash_basics/test4.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# assigning a variable value to another variable
+
+value1=10
+value2=$value1
+echo the resulting value is $value2
+
