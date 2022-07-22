3a7ce98b01c6d537556d09ebeaa769d33c58f70a
laurentstaub <laurent.staub@gmail.com>
2022-07-22 11:36:10 +0200

    JS175 contacts project

---
+++ JS175_networked_applications/my-first-express-app/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'pug');
+
+app.get('/', (request, response) => {
+  response.render('index');
+});
+
+app.get('/account', (request, response) => {
+  response.render('account', { money: '$100', recentTransaction: true });
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000.");
+});
